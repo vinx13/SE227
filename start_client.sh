eadb2678b2dea2b9f837498a531dd09570723380
Wuwei Lin <vincentl13x@gmail.com>
2017-10-19 16:18:02 +0800

    Resolve conflict

--- start_client.sh
+++ start_client.sh
@@ -12,14 +12,10 @@ BASE_PORT=$[BASE_PORT+2000]
 EXTENT_PORT=$BASE_PORT
 YFS1_PORT=$[BASE_PORT+2]
 YFS2_PORT=$[BASE_PORT+4]
-<<<<<<< HEAD
-EXTENT_SERVER_HOST=172.17.0.4
-=======
 LOCK_PORT=$[BASE_PORT+6]
 #Please set this two server IPs 
 EXTENT_SERVER_HOST=127.0.0.1
 LOCK_SERVER_HOST=127.0.0.1
->>>>>>> cf4284c1e97487806b378590fcf15de310910818
 
 YFSDIR1=$PWD/yfs1
 YFSDIR2=$PWD/yfs2
@@ -27,25 +23,15 @@ YFSDIR2=$PWD/yfs2
 rm -rf $YFSDIR1
 mkdir $YFSDIR1 || exit 1
 sleep 1
-<<<<<<< HEAD
-echo "starting ./yfs_client $YFSDIR1 $EXTENT_SERVER_HOST:$EXTENT_PORT > yfs_client1.log 2>&1 &"
-./yfs_client $YFSDIR1 $EXTENT_SERVER_HOST:$EXTENT_PORT > yfs_client1.log 2>&1 &
-=======
 echo "starting ./yfs_client $YFSDIR1 $EXTENT_SERVER_HOST:$EXTENT_PORT $LOCK_SERVER_HOST:$LOCK_PORT > yfs_client1.log 2>&1 &"
 ./yfs_client $YFSDIR1 $EXTENT_SERVER_HOST:$EXTENT_PORT $LOCK_SERVER_HOST:$LOCK_PORT > yfs_client1.log 2>&1 &
->>>>>>> cf4284c1e97487806b378590fcf15de310910818
 sleep 1
 
 rm -rf $YFSDIR2
 mkdir $YFSDIR2 || exit 1
 sleep 1
-<<<<<<< HEAD
-echo "starting ./yfs_client $YFSDIR2 $EXTENT_SERVER_HOST:$EXTENT_PORT > yfs_client2.log 2>&1 &"
-./yfs_client $YFSDIR2 $EXTENT_SERVER_HOST:$EXTENT_PORT > yfs_client2.log 2>&1 &
-=======
 echo "starting ./yfs_client $YFSDIR2 $EXTENT_SERVER_HOST:$EXTENT_PORT $LOCK_SERVER_HOST:$LOCK_PORT > yfs_client2.log 2>&1 &"
 ./yfs_client $YFSDIR2 $EXTENT_SERVER_HOST:$EXTENT_PORT $LOCK_SERVER_HOST:$LOCK_PORT > yfs_client2.log 2>&1 &
->>>>>>> cf4284c1e97487806b378590fcf15de310910818
 
 sleep 2
 
